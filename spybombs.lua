92f73ad69cee1f53e602653adb14efaab896d827
jet-bird <leonid.krashenko@gmail.com>
2015-04-20 00:09:49 +0300

    Initial commit

---
+++ spybombs.lua
@@ -0,0 +1,210 @@
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+--
+-- file:    spybombs.lua
+-- brief:
+-- author:  Leonid Krashenko <leonid.krashenko@gmail.com>
+--
+-- Copyright (C) 2014.
+-- Licensed under the terms of the GNU GPL, v2.
+--
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+function widget:GetInfo()
+	return {
+		name = "Spy Bombs",
+		desc = "Tells spy-bots to destroy themselfs if there is enemy crowd nearby.",
+		author = "jetbird",
+		date = "Apr 19, 2015",
+		license = "GNU GPL, v2",
+		layer = 0,
+		enabled = true --  loaded by default?
+	}
+end
+
+
+local glColor = gl.Color
+local glDepthTest = gl.DepthTest
+local glBeginEnd = gl.BeginEnd
+local glLineWidth = gl.LineWidth
+local glShape = gl.Shape
+local glDrawGroundCircle = gl.DrawGroundCircle
+local GetUnitDefID = Spring.GetUnitDefID
+local spGetAllUnits = Spring.GetAllUnits
+local spGetSpectatingState = Spring.GetSpectatingState
+local spGetMyPlayerID = Spring.GetMyPlayerID
+local spGetPlayerInfo = Spring.GetPlayerInfo
+local spGetMyTeamID = Spring.GetMyTeamID
+local spGetTeamUnits = Spring.GetTeamUnits
+local spGetUnitDefID = Spring.GetUnitDefID
+local spGiveOrderToUnitMap = Spring.GiveOrderToUnitMap
+local spGetGroundInfo = Spring.GetGroundInfo
+local spGetGroundHeight = Spring.GetGroundHeight
+local spGetUnitsInCylinder = Spring.GetUnitsInCylinder
+local echo = Spring.Echo
+
+local units = {} -- player's units
+local armSpyUDId = UnitDefNames["armspy"].id
+local coreSpyUDId = UnitDefNames["corspy"].id
+local targets = {}
+local spyTimeToBlast = {}
+
+local function print_array(A, title)
+	local s = "";
+	if title ~= nil then
+		s = s .. title .. ": ";
+	end;
+	s = s .. "[";
+
+	for k, v in pairs(A) do
+		s = s .. tonumber(v - 1)
+		if (k ~= #A) then
+			s = s .. ", ";
+		end;
+	end;
+	s = s .. "]";
+	print(s);
+	Spring.Echo(s)
+end
+local function print_matrix(m, title)
+	if title ~= nil then echo(title) end
+	for j = 1, #m do
+		echo (j.."  "..table.concat(m[j], ", "))
+	end
+	echo('')
+end
+local function print_freemexes()
+	echo("Free mexes: ")
+	for i, mexpos in ipairs(free_mexes) do
+		echo("mex "..i.." - "..mexpos[1]..", "..mexpos[2])
+	end
+	echo('')
+end
+local function print_map(m, title)
+	for k, v in pairs(m) do
+		echo (title.."["..k.."]".." = "..v)
+	end
+end
+
+function widget:DrawWorldPreUnit()
+	glLineWidth(3.0)
+	glDepthTest(true)
+	glColor(1, 0, 0, .2)
+	for id, v in pairs(units) do
+		local posx,posy,posz = Spring.GetUnitPosition(id)
+		gl.Color(0.5, 0.5, 0.5, 0.5);
+		gl.DrawGroundCircle(posx, posy, posz, v, 25)
+	end
+
+--	glColor(0, 1, 0, .3)
+--	for i, pos in ipairs(local_mexes) do
+--		glDrawGroundCircle(pos[1], 20, pos[2], 50, 16)
+--	end
+--
+--	glColor(0, 0, 1, .3)
+--	for i, pos in ipairs(free_mexes) do
+--		glDrawGroundCircle(pos[1], 20, pos[2], 50, 16)
+--	end
+
+	glDepthTest(false)
+end
+
+
+function widget:DrawScreenEffects()
+	
+    for id, v in pairs(units) do
+        local x,y=Spring.WorldToScreenCoords(Spring.GetUnitPosition(id))
+		local time = spyTimeToBlast[id]
+		if time == nil then time = "" end
+        gl.Text("ID:"..v..": ".." -- "..time,x,y,16,"od")
+    end
+end
+
+local function dispatchUnit(unitID, unitDefID)
+	--local ud = UnitDefs[unitDefID]
+	if unitDefID == armSpyUDId or unitDefID == coreSpyUDId then
+		local udef = UnitDefs[unitDefID]
+		local selfdBlastId = WeaponDefNames[string.lower(udef["selfDExplosion"])].id
+		local selfdBlastRadius = WeaponDefs[selfdBlastId]["damageAreaOfEffect"]
+		units[unitID] = selfdBlastRadius
+		spyTimeToBlast[unitID] = 0
+		echo ("spy detected "..selfdBlastRadius)
+	end
+end
+
+function widget:UnitCreated(unitID, unitDefID, unitTeam)
+	if (unitTeam ~= spGetMyTeamID()) then
+		return
+	end
+end
+
+function widget:Update(dt)
+	for id, v in pairs(units) do
+		local posx,posy,posz = Spring.GetUnitPosition(id)
+		targets[id] = Spring.GetUnitsInSphere(posx, posy, posz, v)
+		
+		local enemies = 0
+		for _, tid in ipairs(targets[id]) do
+			if Spring.GetUnitTeam(tid) ~= spGetMyTeamID() then
+				enemies = enemies + 1
+			end
+		end
+		
+		if enemies > 2 then
+			spyTimeToBlast[id] = spyTimeToBlast[id] + dt
+			if spyTimeToBlast[id] > 0.5 then 
+				Spring.GiveOrderToUnit(id, CMD.SELFD, {}, {})
+				units[id] = nil
+				spyTimeToBlast[id] = nil
+			end
+		else
+			spyTimeToBlast[id] = 0
+		end
+		
+	end
+
+end
+
+function widget:GameFrame(frameNum)
+	if (frameNum % 2 ) == 0 then
+		
+	end
+end
+
+function widget:UnitFinished(unitID, unitDefID, unitTeam)
+	if (unitTeam ~= spGetMyTeamID()) then
+		return
+	end
+	dispatchUnit(unitID, unitDefID)
+end
+
+function widget:UnitDestroyed(unitID, unitDefID, unitTeam)
+	units[unitID] = nil
+end
+
+
+function widget:UnitTaken(unitID, unitDefID, unitTeam, newTeam)
+	widget:UnitDestroyed(unitID, unitDefID)
+end
+
+
+function widget:UnitGiven(unitID, unitDefID, unitTeam, oldTeam)
+	widget:UnitFinished(unitID, unitDefID, unitTeam)
+end
+
+
+function widget:Initialize()
+																																						
+	local playerID = spGetMyPlayerID()
+	local _, _, spec, _, _, _, _, _ = spGetPlayerInfo(playerID)
+	if spec == true then
+		Spring.Echo("<Spy Bombs> Spectator mode. Widget removed")
+		widgetHandler:RemoveWidget(self)
+	end
+
+	local allunits = spGetTeamUnits(spGetMyTeamID())
+	for _, uid in ipairs(allunits) do
+		dispatchUnit(uid, spGetUnitDefID(uid))
+	end
+end
+
